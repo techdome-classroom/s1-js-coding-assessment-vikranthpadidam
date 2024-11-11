ac13604441300051583c2d5389ed78b1d69cc89f
Vikranth Padidam <121481425+vikranthpadidam@users.noreply.github.com>
2024-11-11 08:21:50 +0000

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,23 +1,29 @@
 const decodeTheRing = function (s, p) {
   let m = s.length, n = p.length;
-  let dp = Array(m + 1).fill(false).map(() => Array(n + 1).fill(false));
-  dp[0][0] = true;
+  let dp = Array(n + 1).fill(false);
+  dp[0] = true;
 
   for (let j = 1; j <= n; j++) {
-      if (p[j - 1] === '*') dp[0][j] = dp[0][j - 1];
+      if (p[j - 1] === '*') dp[j] = dp[j - 1];
   }
 
   for (let i = 1; i <= m; i++) {
+      let prev = dp[0];
+      dp[0] = false;
       for (let j = 1; j <= n; j++) {
+          let temp = dp[j];
           if (p[j - 1] === s[i - 1] || p[j - 1] === '?') {
-              dp[i][j] = dp[i - 1][j - 1];
+              dp[j] = prev;
           } else if (p[j - 1] === '*') {
-              dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+              dp[j] = dp[j - 1] || dp[j];
+          } else {
+              dp[j] = false;
           }
+          prev = temp;
       }
   }
 
-  return dp[m][n];
+  return dp[n];
 };
 
 module.exports = decodeTheRing;
