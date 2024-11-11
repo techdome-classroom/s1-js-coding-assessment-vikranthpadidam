d2adec966afa0d7d220484d67f5b000cd332cde4
Vikranth Padidam <121481425+vikranthpadidam@users.noreply.github.com>
2024-11-11 08:30:26 +0000

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,29 +1,29 @@
-const decodeTheRing = function (s, p) {
-  let m = s.length, n = p.length;
-  let dp = Array(n + 1).fill(false);
-  dp[0] = true;
+const matchPattern = function (message, pattern) {
+  let messageLength = message.length, patternLength = pattern.length;
+  let dpArray = Array(patternLength + 1).fill(false);
+  dpArray[0] = true;
 
-  for (let j = 1; j <= n; j++) {
-      if (p[j - 1] === '*') dp[j] = dp[j - 1];
+  for (let j = 1; j <= patternLength; j++) {
+      if (pattern[j - 1] === '*') dpArray[j] = dpArray[j - 1];
   }
 
-  for (let i = 1; i <= m; i++) {
-      let prev = dp[0];
-      dp[0] = false;
-      for (let j = 1; j <= n; j++) {
-          let temp = dp[j];
-          if (p[j - 1] === s[i - 1] || p[j - 1] === '?') {
-              dp[j] = prev;
-          } else if (p[j - 1] === '*') {
-              dp[j] = dp[j - 1] || dp[j];
+  for (let i = 1; i <= messageLength; i++) {
+      let prevValue = dpArray[0];
+      dpArray[0] = false;
+      for (let j = 1; j <= patternLength; j++) {
+          let tempValue = dpArray[j];
+          if (pattern[j - 1] === message[i - 1] || pattern[j - 1] === '?') {
+              dpArray[j] = prevValue;
+          } else if (pattern[j - 1] === '*') {
+              dpArray[j] = dpArray[j - 1] || dpArray[j];
           } else {
-              dp[j] = false;
+              dpArray[j] = false;
           }
-          prev = temp;
+          prevValue = tempValue;
       }
   }
 
-  return dp[n];
+  return dpArray[patternLength];
 };
 
-module.exports = decodeTheRing;
+module.exports = matchPattern;
